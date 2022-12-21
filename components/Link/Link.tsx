c27f34c9f92a5eff87d6773655eac7c8b86469ea
scd7896 <scd1212@naver.com>
2022-12-21 18:31:59 +0900

    fix: Link function displayname 설정

--- components/Link/Link.tsx
+++ components/Link/Link.tsx
@@ -1,11 +1,11 @@
-import Link from "next/link";
+import NextLink from "next/link";
 import React from "react";
 import styles from "./Link.module.css";
 
-export default function (props: React.AnchorHTMLAttributes<HTMLAnchorElement>) {
+export default function Link(props: React.AnchorHTMLAttributes<HTMLAnchorElement>) {
 	return (
-		<Link href={props.href || ""} {...props} className={`${props.className}`}>
+		<NextLink href={props.href || ""} {...props} className={`${props.className}`}>
 			<span className={`${styles.anchor}`}>{props.children}</span>
-		</Link>
+		</NextLink>
 	);
 }
