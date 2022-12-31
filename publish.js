f683621729cf04b7b24d8a02a2ebd0c4a15de892
scd7896 <scd1212@naver.com>
2022-12-31 15:36:03 +0900

    fix: accessKEyId 키값 설정

--- publish.js
+++ publish.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const s3 = new AWS.S3({
 	region: "ap-northeast-2",
-	accessKeyId: process.env.AWS_KEY_ID,
+	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
 	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
@@ -53,7 +53,7 @@ const uploadDir = function (s3Path, bucketName) {
 s3.listObjects({ Bucket: "3xuxcard" }, (err, data) => {
 	if (err) {
 		console.log(err);
-		console.log(process.env.AWS_KEY_ID === undefined);
+		console.log(process.env.AWS_ACCESS_KEY_ID === undefined);
 	}
 	s3.deleteObjects(
 		{
