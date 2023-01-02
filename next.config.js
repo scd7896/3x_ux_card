8d506b9004d81fa6167bdb065dd5511cefc9cec2
scd7896 <scd1212@naver.com>
2023-01-02 20:36:29 +0900

    fix: basePath 버그 수정

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const basePath = {
-	development: "",
-	production: "/",
-};
-
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
-	basePath: basePath[process.env.NODE_ENV],
 };
 
 module.exports = nextConfig;
