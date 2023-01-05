a051679e526823475e8aa070bf0d6cc4752bd3ce
scd7896 <scd1212@naver.com>
2023-01-05 17:21:34 +0900

    feat: 배너 작성 완료

---
+++ components/services/home/Banner.tsx
@@ -0,0 +1,20 @@
+import Button from "../../Button/Button";
+import { ButtonText } from "../../text/Text";
+import styles from "./Banner.module.css";
+
+export default function HoneBanner() {
+	return (
+		<section className={styles.wrapper}>
+			<div className={styles.bannerTextWrapper}>
+				<section className={styles.bannerText}>차근차근 하나씩 하다보면</section>
+				<section className={styles.bannerText}>UX 전문가가 되어있을 거에요</section>
+			</div>
+			<div className={styles.buttonWrapper}>
+				<Button color="line">
+					<ButtonText level={1}>3x 이용방법</ButtonText>
+				</Button>
+			</div>
+			<div className={styles.imgWrapper}></div>
+		</section>
+	);
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
 import PageLayout from "../components/layout/PageLayout";
+import HoneBanner from "../components/services/home/Banner";
 
 export default function Home() {
-	return <PageLayout></PageLayout>;
+	return (
+		<PageLayout>
+			<HoneBanner />
+		</PageLayout>
+	);
 }
