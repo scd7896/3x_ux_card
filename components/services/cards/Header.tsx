0fc72840c333af57fef03fd0ed2ad9cb73fce9ad
scd7896 <scd1212@naver.com>
2023-01-09 22:44:10 +0900

    feat: StepTab 카드페이지 헤더에 붙임

--- components/services/cards/Header.tsx
+++ components/services/cards/Header.tsx
@@ -1,3 +1,4 @@
+import StepTab from "../../Tab/StepTab";
 import Title from "../../text/Title";
 import styles from "./Header.module.css";
 
@@ -5,7 +6,20 @@ export default function CardsHeader() {
 	return (
 		<div className={styles.wrapper}>
 			<section className={styles.contentWrapper}>
-				<Title level={1}>Cards</Title>
+				<section className={styles.titleWrapper}>
+					<Title level={1}>Cards</Title>
+				</section>
+				<StepTab
+					steps={[
+						{ key: "전체" },
+						{ key: "공감" },
+						{ key: "아이디어" },
+						{ key: "프로토타입" },
+						{ key: "테스트" },
+						{ key: "출시" },
+					]}
+					arrowStartIndex={1}
+				/>
 			</section>
 		</div>
 	);
