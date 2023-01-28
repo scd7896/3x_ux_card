1eee22e2865f153ccf0e1a497af3a2c1858dce6b
scd7896 <scd1212@naver.com>
2023-01-28 17:29:36 +0900

    test: 프로세스별 필터 테스트코드 작성

--- cypress/pages/cardList/indexCardsPage.cy.tsx
+++ cypress/pages/cardList/indexCardsPage.cy.tsx
@@ -8,10 +8,15 @@ describe("<CardsPage />", () => {
 		cy.intercept({ method: "GET", url: "**/list.json**" }, cardListMock).as("cardListMock");
 	});
 
-	it("renders", () => {
+	it("프로세스별 보기에서 공감을 누르면 공감만 노출되어야 한다.", () => {
 		cy.mount(<CardsPage />);
 		cy.wait("@cardListMock");
 
-		cy.get("[data-cy=]");
+		cy.get("[data-cy=process_filter]").click();
+		cy.get('[data-cy="공감"]').click();
+
+		cy.get("[data-cy=card_wrapper]")
+			.find("[data-cy=card]")
+			.should("have.length", cardListMock.filter((it) => it.data.process === "공감").length);
 	});
 });
