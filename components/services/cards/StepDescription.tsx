c640c32bdda5216dbe7a1583035b71b936db4b4f
scd7896 <scd1212@naver.com>
2023-01-29 21:53:48 +0900

    feat: 카드 설명 부분 간격 및 border 작성

--- components/services/cards/StepDescription.tsx
+++ components/services/cards/StepDescription.tsx
@@ -3,6 +3,8 @@ import { CardSubTitleDescription } from "../../../lib/constant";
 import Body from "../../text/Body";
 import Title from "../../text/Title";
 
+import styles from "./StepDescription.module.css";
+
 interface IProp {
 	descriptionKey?: string;
 }
@@ -17,8 +19,10 @@ export default function StepDescription({ descriptionKey }: IProp) {
 	}, [descriptionKey]);
 
 	return (
-		<section>
-			<Title level={3}>{descriptionKey || "전체"}</Title>
+		<section className={styles.wrapper}>
+			<div className={styles.titleWrapper}>
+				<Title level={3}>{descriptionKey || "전체"}</Title>
+			</div>
 			<Body level={1} data-cy="description">
 				{description}
 			</Body>
