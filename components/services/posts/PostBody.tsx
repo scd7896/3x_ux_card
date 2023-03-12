c3ddc036cde375c89c47868a4218f44842f3f833
scd7896 <scd1212@naver.com>
2023-03-12 16:50:41 +0900

    feat: 링크 복사 버튼 구현

--- components/services/posts/PostBody.tsx
+++ components/services/posts/PostBody.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import ArrowLeft from "../../icon/ArrowLeft";
 import Link from "../../Link/Link";
 import styles from "./PostBody.module.css";
@@ -7,14 +8,30 @@ interface IProp {
 }
 
 export default function PostBody({ contentHtml }: IProp) {
+	const copyClickHandler = useCallback((e: any) => {
+		e.preventDefault();
+
+		const href = window.location.href;
+		var tempElem = document.createElement("textarea");
+		tempElem.value = href;
+		document.body.appendChild(tempElem);
+		tempElem.select();
+		document.execCommand("copy");
+		document.body.removeChild(tempElem);
+	}, []);
+
 	return (
 		<section className={styles.wrapper}>
 			<div className={styles.contentWrapper}>
 				<div className={styles.content} dangerouslySetInnerHTML={{ __html: contentHtml }}></div>
 				<section className={styles.linkWrapper}>
-					<Link href="/cards" style={{ fontSize: "16px" }}>
+					<Link href="/cards">
 						<ArrowLeft />
 						목록으로
+					</Link>
+
+					<Link href="#" onClick={copyClickHandler}>
+						링크 복사
 					</Link>
 				</section>
 			</div>
