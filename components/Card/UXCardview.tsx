b2e41cc3eab53c8fdd6e5b3313935a29f4c2ca6c
scd7896 <scd1212@naver.com>
2023-06-03 17:12:59 +0900

    카드 호버시 뒷면 보이도록 함

--- components/Card/UXCardview.tsx
+++ components/Card/UXCardview.tsx
@@ -1,24 +1,41 @@
 import { ICard } from "../../types/card.d";
+import Body from "../text/Body";
 import SubTitle from "../text/SubTitle";
 import { Caption } from "../text/Text";
 import Title from "../text/Title";
 import styles from "./UXCardview.module.css";
 
-export default function UXCardview({ title, enTitle, members, icon, process }: ICard) {
+export default function UXCardview({ title, enTitle, members, icon, process, description, level, duration }: ICard) {
 	return (
-		<div className={`${styles.wrapper} ${styles[process]}`}>
-			<section>
-				<Caption>{enTitle}</Caption>
-				<Title level={3}>{title}</Title>
-			</section>
-			<section className={styles.bottomWrapper}>
-				<div className={styles.badgeWrapper}>
-					{members?.map((it) => (
-						<Badge key={it} process={process} member={it} />
-					))}
-				</div>
-			</section>
-		</div>
+		<section className={`${styles.container} ${styles[process]}`}>
+			<div className={`${styles.wrapper}`}>
+				<section>
+					<Caption>{enTitle}</Caption>
+					<Title level={3}>{title}</Title>
+				</section>
+				<section className={styles.bottomWrapper}>
+					<div className={styles.badgeWrapper}>
+						{members?.map((it) => (
+							<Badge key={it} process={process} member={it} />
+						))}
+					</div>
+				</section>
+			</div>
+			<div className={`${styles.backWrapper}`}>
+				<section>
+					<Caption>{enTitle}</Caption>
+					<Title level={3} className={styles.backTitle}>
+						{title}
+					</Title>
+					<Body level={3}>{description}</Body>
+				</section>
+				<section>
+					<Caption>소요시간 / {duration}</Caption>
+					<br />
+					<Caption>난이도 / {level}</Caption>
+				</section>
+			</div>
+		</section>
 	);
 }
 
