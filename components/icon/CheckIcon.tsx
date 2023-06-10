0e10092dbb23a3c318d0760039fed9cbd8158872
scd7896 <scd1212@naver.com>
2023-06-10 18:58:54 +0900

    노티피케이션 추가

---
+++ components/icon/CheckIcon.tsx
@@ -0,0 +1,13 @@
+export default function CheckIcon() {
+	return (
+		<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path
+				d="M5 11.4L10.3846 17L20 7"
+				stroke="white"
+				stroke-width="1.5"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			/>
+		</svg>
+	);
+}
---
+++ lib/notification.tsx
@@ -0,0 +1,35 @@
+import styles from "./notification.module.css";
+import { createRoot } from "react-dom/client";
+import CheckIcon from "../components/icon/CheckIcon";
+
+interface IArgsProps {
+	message: React.ReactNode;
+	type?: "check";
+}
+
+const iconMap = {
+	check: <CheckIcon />,
+};
+
+function Notification({ message, type }: IArgsProps) {
+	return (
+		<div className={styles.wrapper}>
+			{type && <div className={styles.iconWrapper}>{iconMap[type]}</div>}
+			{message}
+		</div>
+	);
+}
+
+export const push = ({ message, type }: IArgsProps) => {
+	const notiElement = document.createElement("div");
+	document.body.appendChild(notiElement);
+
+	const root = createRoot(notiElement);
+
+	root.render(<Notification message={message} type={type} />);
+
+	setTimeout(() => {
+		root.unmount();
+		notiElement.remove();
+	}, 1000);
+};
