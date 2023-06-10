b43932a7d0c1c2934f1f43491cb1682c071fb6d1
scd7896 <scd1212@naver.com>
2023-06-10 18:21:54 +0900

    메일 아이콘 추가

---
+++ components/icon/MailIcon.tsx
@@ -0,0 +1,20 @@
+export default function MailIcon() {
+	return (
+		<svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<circle opacity="0.2" cx="32" cy="32" r="32" fill="#D9D6D1" />
+			<path
+				d="M44 21H20C18.8954 21 18 21.8954 18 23V41.82C18 42.9246 18.8954 43.82 20 43.82H44C45.1046 43.82 46 42.9246 46 41.82V23C46 21.8954 45.1046 21 44 21Z"
+				stroke="#333333"
+				stroke-width="1.5"
+				stroke-miterlimit="10"
+			/>
+			<path
+				d="M18 24.0703L31.81 33.1503L46 24.0703"
+				stroke="#333333"
+				stroke-width="1.5"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			/>
+		</svg>
+	);
+}
