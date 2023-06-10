0077edc2a663f4ac582698cb42426817b00ea07c
scd7896 <scd1212@naver.com>
2023-06-10 17:27:27 +0900

    모달 내용 작성

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,12 +2,12 @@ import styles from "./Button.module.css";
 
 interface IProp extends React.HTMLAttributes<HTMLButtonElement> {
 	size?: "small" | "middle" | "default";
-	color?: "solid" | "line";
+	color?: "solid" | "line" | "ghost";
 }
 
 export default function Button({ color = "line", size = "default", children, ...props }: IProp) {
 	return (
-		<button className={`${styles.button} ${styles[color]} ${styles[size]}`} {...props}>
+		<button {...props} className={`${styles.button} ${styles[color]} ${styles[size]} ${props.className}`}>
 			{children}
 		</button>
 	);
--- components/services/cards/MemberFilter.tsx
+++ components/services/cards/MemberFilter.tsx
@@ -1,5 +1,8 @@
 import { useState, useCallback, useEffect } from "react";
 import { members } from "../../../lib/constant";
+import Button from "../../Button/Button";
+import Modal from "../../Modal/Modal";
+import Body from "../../text/Body";
 import SubTitle from "../../text/SubTitle";
 
 import styles from "./MemberFilter.module.css";
@@ -9,6 +12,7 @@ interface IProp {
 }
 
 export default function MemberFilter({ onChange }: IProp) {
+	const [modalOpen, setModalOpen] = useState(false);
 	const [selectedMembers, setSelectedMemebers] = useState<string[]>([]);
 
 	const getSelected = useCallback(
@@ -24,7 +28,9 @@ export default function MemberFilter({ onChange }: IProp) {
 
 	return (
 		<section className={styles.wrapper}>
-			<SubTitle level={1}>참여자</SubTitle>
+			<SubTitle level={1}>
+				참여자 <button onClick={() => setModalOpen(true)}>인포</button>
+			</SubTitle>
 			<div className={styles.membersWrapper}>
 				{members.map((it) => (
 					<button
@@ -45,6 +51,31 @@ export default function MemberFilter({ onChange }: IProp) {
 					</button>
 				))}
 			</div>
+			<Modal isOpen={modalOpen} onClose={() => setModalOpen(false)} title="참여자 정의">
+				<div className={styles.memberDefineWrapper}>
+					<section className={styles.memberDefined}>
+						<SubTitle level={2}>UXUI 전문가</SubTitle>
+						<Body className={styles.memberText} level={3}>
+							프로덕트 디자이너, UX 기획자 등 사용자 경험/환경 분야에 전문성을 가진 사람
+						</Body>
+					</section>
+					<section className={styles.memberDefined}>
+						<SubTitle level={2}>팀 구성원</SubTitle>
+						<Body className={styles.memberText} level={3}>
+							개발자 등 UXUI 전문가 이외에 함꼐 프로덕트를 만들어가는 팀 구성원
+						</Body>
+					</section>
+					<section className={styles.memberDefined}>
+						<SubTitle level={2}>이해 관계자</SubTitle>
+						<Body className={styles.memberText} level={3}>
+							같은 팀 구성원은 아니지만 업무 영향을 받는 유관부서 외주 프로젝트일 경우 고객사
+						</Body>
+					</section>
+					<Button color="ghost" className={styles.checkButton} onClick={() => setModalOpen(false)}>
+						확인
+					</Button>
+				</div>
+			</Modal>
 		</section>
 	);
 }
