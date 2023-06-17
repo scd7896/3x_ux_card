afa769f366d72ab2d165ab9b82cb4437aca0ae8f
scd7896 <scd1212@naver.com>
2023-06-17 22:30:15 +0900

    header 선택한 요소 가시화

--- components/layout/HeaderNav.tsx
+++ components/layout/HeaderNav.tsx
@@ -1,23 +1,40 @@
+import { useRouter } from "next/router";
+import { useCallback } from "react";
 import MainGnb from "../icon/MainGnb";
 import Link from "../Link/Link";
+import Body from "../text/Body";
+import SubTitle from "../text/SubTitle";
 import { ButtonText } from "../text/Text";
 import styles from "./HeaderNav.module.css";
 
 export default function HeaderNav() {
+	const { pathname } = useRouter();
+
+	const getText = useCallback(
+		(keyString: string, text: string) => {
+			if (keyString === pathname) {
+				return <SubTitle level={2}>{text}</SubTitle>;
+			} else {
+				return <Body level={2}>{text}</Body>;
+			}
+		},
+		[pathname]
+	);
+
 	return (
 		<header className={styles.header}>
 			<nav className={styles.nav}>
-				<section className={styles.gnbWrapper}>
+				<Link href="/" className={styles.link}>
 					<MainGnb />
-				</section>
-				<Link className={styles.link} href="/howtouse">
-					<ButtonText level={1}>How to use</ButtonText>
 				</Link>
-				<Link className={styles.link} href="/cards">
-					<ButtonText level={1}>Cards</ButtonText>
+				<Link className={`${styles.link} ${pathname === "/howtouse" && styles.selected}`} href="/howtouse">
+					{getText("/howtouse", "How to use")}
 				</Link>
-				<Link className={styles.link} href="/contact">
-					<ButtonText level={1}>Contact</ButtonText>
+				<Link className={`${styles.link} ${pathname === "/cards" && styles.selected}`} href="/cards">
+					{getText("/cards", "Cards")}
+				</Link>
+				<Link className={`${styles.link} ${pathname === "/contact" && styles.selected}`} href="/contact">
+					{getText("/contact", "Contact")}
 				</Link>
 			</nav>
 		</header>
