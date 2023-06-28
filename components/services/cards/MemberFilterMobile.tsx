318f8b6d3d194f6aa2ead0eff5d49ba77e14b8ee
scd7896 <scd1212@naver.com>
2023-06-28 21:50:25 +0900

    참여자 정의 컴포넌트 개발

--- components/services/cards/MemberFilterMobile.tsx
+++ components/services/cards/MemberFilterMobile.tsx
@@ -6,6 +6,8 @@ import { ArrowDown16 } from "../../icon/ArrowLeft";
 import styles from "./MemberFilterMobile.module.css";
 import MemberInfoIcon from "../../icon/MemberInfoIcon";
 import { CheckIconBlack } from "../../icon/CheckIcon";
+import SubTitle from "../../text/SubTitle";
+import Body from "../../text/Body";
 
 interface IProp {
 	onChange?: (members: string[]) => void;
@@ -13,6 +15,7 @@ interface IProp {
 
 export default function MemberFilterMobile({ onChange }: IProp) {
 	const [selectOpen, setSelectOpen] = useState(false);
+	const [defineOpen, setDefineOpen] = useState(false);
 	const [selectedMembers, setSelectedMemebers] = useState<string[]>([]);
 
 	const getSelected = useCallback(
@@ -37,10 +40,33 @@ export default function MemberFilterMobile({ onChange }: IProp) {
 					</section>
 				</div>
 
-				<span className={styles.infoIcon}>
+				<span className={styles.infoIcon} onClick={() => setDefineOpen(true)}>
 					<MemberInfoIcon />
 				</span>
 			</div>
+			<Modal position="bottom" title="참여자 정의" isOpen={defineOpen} onClose={() => setDefineOpen(false)}>
+				<section className={styles.defineWrapper}>
+					<SubTitle level={3} className={styles.defineLabel}>
+						UXUI 전문가
+					</SubTitle>
+					<Body level={2}>프로덕트 디자이너, UX 기획자 등 사용자 경험/환경 분야에 전문성을 가진 사람</Body>
+				</section>
+				<section className={styles.defineWrapper}>
+					<SubTitle level={3} className={styles.defineLabel}>
+						팀 구성원
+					</SubTitle>
+					<Body level={2}>개발자 등 UXUI 전문가 이외에 함께 프로덕트를 만들어가는 팀 구성원</Body>
+				</section>
+				<section className={styles.defineWrapper}>
+					<SubTitle level={3} className={styles.defineLabel}>
+						이해관계자
+					</SubTitle>
+					<Body level={2}>같은 팀 구성원은 아니지만 프로젝트에 간접적 영향 받는 사람들 또는 고객사 등</Body>
+				</section>
+				<Button color="ghost" className={styles.confirmButton} onClick={() => setDefineOpen(false)}>
+					확인
+				</Button>
+			</Modal>
 			<Modal position="bottom" isOpen={selectOpen} title="참여자 선택" onClose={() => setSelectOpen(false)}>
 				<div className={styles.membersWrapper}>
 					{members.map((it) => (
