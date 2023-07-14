df1b0ee08924c61d959b5fc834062d78ce55e6e0
scd7896 <scd1212@naver.com>
2023-07-14 14:25:23 +0900

    링크 수정

--- lib/env.ts
+++ lib/env.ts
@@ -1,4 +1,4 @@
 export const IS_PRODUCTION = process.env.NODE_ENV === "production";
 
-export const handbookUrl = "https://drive.google.com/drive/folders/1g6hCNsP-gc5vVlnzP8oitws7Zu9pzu5Z";
-export const worksheetUrl = "https://drive.google.com/drive/folders/1g6hCNsP-gc5vVlnzP8oitws7Zu9pzu5Z";
+export const handbookUrl = "https://drive.google.com/file/d/1IoRsHdvUPeNKmNsIbMOT2pYCeL-eh4Id/view";
+export const worksheetUrl = "https://drive.google.com/file/d/17KfNn7muyV7SfM221QMvJz27adYW_KUn/view?usp=sharing";
