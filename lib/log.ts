1fad32bedf953cb1290283f2d5b5534e5797f722
scd7896 <scd1212@naver.com>
2023-09-26 22:23:03 +0900

    콘솔 잘못찍는 오류 수정

--- lib/log.ts
+++ lib/log.ts
@@ -1,181 +1,178 @@
 class GALogger {
-  clickHomeTopBanner() {
-    (window as any).dataLayer.push({
-      event: "custom_click",
-      value: "click_home_topbanner",
-    });
-  }
+	clickHomeTopBanner() {
+		(window as any).dataLayer.push({
+			event: "custom_click",
+			value: "click_home_topbanner",
+		});
+	}
 
-  clickGnbLogo() {
-    (window as any).dataLayer.push({
-      event: "custom_click",
-      value: "click_gnb_logo",
-    });
-  }
+	clickGnbLogo() {
+		(window as any).dataLayer.push({
+			event: "custom_click",
+			value: "click_gnb_logo",
+		});
+	}
 
-  clickGnbHowtouse() {
-    (window as any).dataLayer.push({
-      event: "custom_click",
-      value: "click_gnb_howtouse",
-    });
-  }
+	clickGnbHowtouse() {
+		(window as any).dataLayer.push({
+			event: "custom_click",
+			value: "click_gnb_howtouse",
+		});
+	}
 
-  clickGnbCards() {
-    (window as any).dataLayer.push({
-      event: "custom_click",
-      value: "click_gnb_cards",
-    });
-  }
+	clickGnbCards() {
+		(window as any).dataLayer.push({
+			event: "custom_click",
+			value: "click_gnb_cards",
+		});
+	}
 
-  clickGnbContact() {
-    (window as any).dataLayer.push({
-      event: "custom_click",
-      value: "click_gnb_contact",
-    });
-  }
+	clickGnbContact() {
+		(window as any).dataLayer.push({
+			event: "custom_click",
+			value: "click_gnb_contact",
+		});
+	}
 
-  clickHomePersonal() {
-    (window as any).dataLayer.push({
-      event: "custom_click",
-      value: "click_home_personal",
-    });
-  }
+	clickHomePersonal() {
+		(window as any).dataLayer.push({
+			event: "custom_click",
+			value: "click_home_personal",
+		});
+	}
 
-  clickHomeRenewal() {
-    (window as any).dataLayer.push({
-      event: "custom_click",
-      value: "click_home_renewal",
-    });
-  }
+	clickHomeRenewal() {
+		(window as any).dataLayer.push({
+			event: "custom_click",
+			value: "click_home_renewal",
+		});
+	}
 
-  clickHomeLaunching() {
-    (window as any).dataLayer.push({
-      event: "custom_click",
-      value: "click_home_lauching",
-    });
-  }
+	clickHomeLaunching() {
+		(window as any).dataLayer.push({
+			event: "custom_click",
+			value: "click_home_lauching",
+		});
+	}
 
-  clickHomePrints() {
-    (window as any).dataLayer.push({
-      event: "custom_click",
-      value: "click_home_prints",
-    });
-  }
+	clickHomePrints() {
+		(window as any).dataLayer.push({
+			event: "custom_click",
+			value: "click_home_prints",
+		});
+	}
 
-  clickHomeCardList() {
-    (window as any).dataLayer.push({
-      event: "custom_click",
-      value: "click_home_cardlist",
-    });
-  }
+	clickHomeCardList() {
+		(window as any).dataLayer.push({
+			event: "custom_click",
+			value: "click_home_cardlist",
+		});
+	}
 
-  clickHowtouseHandbookDownload() {
-    (window as any).dataLayer.push({
-      event: "custom_click",
-      value: "click_howtouse_handbook_download",
-    });
-  }
+	clickHowtouseHandbookDownload() {
+		(window as any).dataLayer.push({
+			event: "custom_click",
+			value: "click_howtouse_handbook_download",
+		});
+	}
 
-  clickHowtouseWorksheetDownload() {
-    (window as any).dataLayer.push({
-      event: "custom_click",
-      value: "click_howtouse_worksheet_download",
-    });
-  }
+	clickHowtouseWorksheetDownload() {
+		(window as any).dataLayer.push({
+			event: "custom_click",
+			value: "click_howtouse_worksheet_download",
+		});
+	}
 
-  clickCardListCategory(category: string) {
-    (window as any).dataLayer.push({
-      event: "custom_click",
-      value: `click_cardlist_filter_category_${category}`,
-    });
-  }
+	clickCardListCategory(category: string) {
+		(window as any).dataLayer.push({
+			event: "custom_click",
+			value: `click_cardlist_filter_category_${category}`,
+		});
+	}
 
-  clickCardListFilterValue(category: string, value: string) {
-    console.log(dataLayer, (window as any).dataLayer);
-    (window as any).dataLayer.push({
-      event: "custom_click",
-      value: `click_cardlist_filter_${category}_${value}`,
-    });
-  }
+	clickCardListFilterValue(category: string, value: string) {
+		(window as any).dataLayer.push({
+			event: "custom_click",
+			value: `click_cardlist_filter_${category}_${value}`,
+		});
+	}
 
-  clickCardListMemberFilter(member: string, isEnable: boolean = true) {
-    (window as any).dataLayer.push({
-      event: "custom_click",
-      value: `click_cardlist_member_${member}_${
-        isEnable ? "enable" : "disable"
-      }`,
-    });
-  }
+	clickCardListMemberFilter(member: string, isEnable: boolean = true) {
+		(window as any).dataLayer.push({
+			event: "custom_click",
+			value: `click_cardlist_member_${member}_${isEnable ? "enable" : "disable"}`,
+		});
+	}
 
-  clickCardList(name: string) {
-    (window as any).dataLayer.push({
-      event: "custom_click",
-      value: `click_cardlist_${name}`,
-    });
-  }
+	clickCardList(name: string) {
+		(window as any).dataLayer.push({
+			event: "custom_click",
+			value: `click_cardlist_${name}`,
+		});
+	}
 
-  clickPostIframe(name: string) {
-    (window as any).dataLayer.push({
-      event: "custom_click",
-      value: `click_carddetail_iframe_${name}`,
-    });
-  }
+	clickPostIframe(name: string) {
+		(window as any).dataLayer.push({
+			event: "custom_click",
+			value: `click_carddetail_iframe_${name}`,
+		});
+	}
 
-  clickCardDetailWorksheetDownload(name: string) {
-    (window as any).dataLayer.push({
-      event: "custom_click",
-      value: `click_carddetail_worksheet_download_${name}`,
-    });
-  }
+	clickCardDetailWorksheetDownload(name: string) {
+		(window as any).dataLayer.push({
+			event: "custom_click",
+			value: `click_carddetail_worksheet_download_${name}`,
+		});
+	}
 
-  clickCardDetailWroksheetGoFigma(name: string) {
-    (window as any).dataLayer.push({
-      event: "custom_click",
-      value: `click_carddetail_worksheet_gofigma_${name}`,
-    });
-  }
+	clickCardDetailWroksheetGoFigma(name: string) {
+		(window as any).dataLayer.push({
+			event: "custom_click",
+			value: `click_carddetail_worksheet_gofigma_${name}`,
+		});
+	}
 
-  clickCardDetailGotoList(name: string) {
-    (window as any).dataLayer.push({
-      event: "custom_click",
-      value: `click_carddetail_gotolist_${name}`,
-    });
-  }
+	clickCardDetailGotoList(name: string) {
+		(window as any).dataLayer.push({
+			event: "custom_click",
+			value: `click_carddetail_gotolist_${name}`,
+		});
+	}
 
-  clickCardDetailShare(name: string) {
-    (window as any).dataLayer.push({
-      event: "custom_click",
-      value: `click_carddetail_share_${name}`,
-    });
-  }
+	clickCardDetailShare(name: string) {
+		(window as any).dataLayer.push({
+			event: "custom_click",
+			value: `click_carddetail_share_${name}`,
+		});
+	}
 
-  clickCardDetailDownloadAllHandbook(name: string) {
-    (window as any).dataLayer.push({
-      event: "custom_click",
-      value: `click_carddetail_download_all_handbook_${name}`,
-    });
-  }
+	clickCardDetailDownloadAllHandbook(name: string) {
+		(window as any).dataLayer.push({
+			event: "custom_click",
+			value: `click_carddetail_download_all_handbook_${name}`,
+		});
+	}
 
-  clickCardDetailDownloadAllWorksheet(name: string) {
-    (window as any).dataLayer.push({
-      event: "custom_click",
-      value: `click_carddetail_download_all_worksheet_${name}`,
-    });
-  }
+	clickCardDetailDownloadAllWorksheet(name: string) {
+		(window as any).dataLayer.push({
+			event: "custom_click",
+			value: `click_carddetail_download_all_worksheet_${name}`,
+		});
+	}
 
-  clickContactMember(member: string) {
-    (window as any).dataLayer.push({
-      event: "custom_click",
-      value: `click_contact_member_${member}`,
-    });
-  }
+	clickContactMember(member: string) {
+		(window as any).dataLayer.push({
+			event: "custom_click",
+			value: `click_contact_member_${member}`,
+		});
+	}
 
-  clickContactMail() {
-    (window as any).dataLayer.push({
-      event: "custom_click",
-      value: `click_contact_member`,
-    });
-  }
+	clickContactMail() {
+		(window as any).dataLayer.push({
+			event: "custom_click",
+			value: `click_contact_member`,
+		});
+	}
 }
 
 const gaLogger = new GALogger();
